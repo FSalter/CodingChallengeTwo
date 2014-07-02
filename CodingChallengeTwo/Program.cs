5f16aa973bf5760b50bcdb69c381797cf40efc9b
Florence Salter <florence.salter@softwire.com>
2014-07-02 18:35:04 +0100

    Second commit

--- CodingChallengeTwo/Program.cs
+++ CodingChallengeTwo/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CodingChallengeTwo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+        var counter = 0;
+        string line;
+        var tls = 0;
+            
+        // Read the file and display it line by line.
+        var file =
+           new StreamReader("tls.txt");
+        while ((line = file.ReadLine()) != null)
+        {
+            counttls.add();
+        }
+        file.Close();
+
+        // Suspend the screen.
+        var readLine = Console.ReadLine();
+        }
+    }
+
+    internal class counttls
+    {
+        public static void add()
+        {
+            throw new NotImplementedException();
         }
     }
 }
